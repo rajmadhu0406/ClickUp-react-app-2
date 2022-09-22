b1e76c9651d94270707cab7db00a4d5ca32c0888
Raj <Raj.madhu0406@gmail.com>
2022-09-22 22:42:22 +0530

    added github-io pages configurations

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Inside App.js in src folder</h1>
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div className="App">
+      <h1>Inside App.js in src folder</h1>
+    </div>
+  );
+}
 
 export default App;
